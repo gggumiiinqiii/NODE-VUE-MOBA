79951cb559f6254c6fcf65ccf775418beb70c968
gggumiiinqiii <1378827345@qq.com>
2021-02-03 21:11:49 +0800

    静态托管文件

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const app = express()
 app.use(require('cors')())
 //在admin/index.js await 中req.body使用
 app.use(express.json())
+//托管静态文件
+app.use('/uploads',express.static(__dirname+'/uploads'))
 // 数据库的连接
 require('./plugins/db')(app)
 //路由的连接
