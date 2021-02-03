5f25863fdb5e1df17916720d81658f0e64e6b331
gggumiiinqiii <1378827345@qq.com>
2021-02-03 21:13:05 +0800

    分类数据库

--- server/modules/Category.js
+++ server/modules/Category.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-  name:{type:String}
+  name:{type:String},
+  // 关联自己本身的模型
+  parent:{type:mongoose.SchemaTypes.ObjectId,ref:'Category'}
 })
 
 module.exports = mongoose.model('Category',schema)
