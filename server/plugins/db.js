290004ff7ea3c5d0ff375f14f2bb00512c7b7df4
gggumiiinqiii <1378827345@qq.com>
2021-02-03 21:14:01 +0800

    中间件解决一个报错

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -3,7 +3,10 @@ module.exports = app =>{
   //eprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. 
   //To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
   //useUnifiedTopology:true 这句话是用来解决上面的报错的
-  mongoose.connect("mongodb://127.0.0.1:27017/node-vue-moba",{useNewUrlParser:true,useUnifiedTopology:true},function(err){
+   //DeprecationWarning: Mongoose: `findOneAndUpdate()` and `findOneAndDelete()` without the `useFindAndModify` option set to false are deprecated.
+  //See: https://mongoosejs.com/docs/deprecations.html#findandmodify
+  //useFindAndModify: false这句话是用来解决上面的报错的
+  mongoose.connect("mongodb://127.0.0.1:27017/node-vue-moba",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify: false},function(err){
       if(err){
         console.log('连接失败')
       }else {
