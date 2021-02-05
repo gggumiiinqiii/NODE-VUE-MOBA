c87a72075f469beec728fef01f9a180a84afcf8d
gggumiiinqiii <1378827345@qq.com>
2021-02-05 09:28:28 +0800

    一些路由的添加

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -9,6 +9,15 @@ import ItemList from '../views/ItemList.vue'
 
 import HeroEdit from '../views/HeroEdit.vue'
 import HeroList from '../views/HeroList.vue'
+
+import ArticleEdit from '../views/ArticleEdit.vue'
+import ArticleList from '../views/ArticleList.vue'
+
+import AdEdit from '../views/AdEdit.vue'
+import AdList from '../views/AdList.vue'
+
+import AdminUserEdit from '../views/AdminUserEdit.vue'
+import AdminUserList from '../views/AdminUserList.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -17,6 +26,7 @@ const routes = [
     name: 'Main',
     component: Main,
     children:[
+      /*分类路由 */
       {
         path:'/categories/create',
         component:CategoryEdit
@@ -31,7 +41,7 @@ const routes = [
         path:'/categories/list',
         component:CategoryList
       },
-
+      /*列表路由 */
       {
         path:'/items/create',
         component:ItemEdit
@@ -46,7 +56,7 @@ const routes = [
         path:'/items/list',
         component:ItemList
       },
-
+      /*英雄路由*/
       {
         path:'/heroes/create',
         component:HeroEdit
@@ -60,6 +70,52 @@ const routes = [
       {
         path:'/heroes/list',
         component:HeroList
+      },
+      /*文章路由*/
+      {
+        path:'/articles/create',
+        component:ArticleEdit
+      },
+      {
+        path:'/articles/edit/:id',
+        component:ArticleEdit,
+        //如果 props 被设置为 true，route.params 将会被设置为组件属性。
+        props:true
+      },
+      {
+        path:'/articles/list',
+        component:ArticleList
+      },
+      /*广告路由*/
+      {
+        path:'/ads/create',
+        component:AdEdit
+      },
+      {
+        path:'/ads/edit/:id',
+        component:AdEdit,
+        //如果 props 被设置为 true，route.params 将会被设置为组件属性。
+        props:true
+      },
+      {
+        path:'/ads/list',
+        component:AdList
+      },
+
+      /*用户路由*/
+      {
+        path:'/admin_users/create',
+        component:AdminUserEdit
+      },
+      {
+        path:'/admin_users/edit/:id',
+        component:AdminUserEdit,
+        //如果 props 被设置为 true，route.params 将会被设置为组件属性。
+        props:true
+      },
+      {
+        path:'/admin_users/list',
+        component:AdminUserList
       },
     ]
   },
