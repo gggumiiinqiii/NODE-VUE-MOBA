6202008f7ee1583eafccdeabb8f9b791d6008c57
gggumiiinqiii <1378827345@qq.com>
2021-02-07 20:41:14 +0800

    请求拦截+响应拦截

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,7 +1,32 @@
 import axios from 'axios'
-
+import Vue from 'vue'
+import router from './router'
 const http = axios.create({
   baseURL:'http://localhost:3000/admin/api'
 })
-
+//请求拦截
+http.interceptors.request.use(config=>{
+  if(localStorage.token) {
+    config.headers.Authorization = 'Bearer '+ (localStorage.token)
+  }
+  return config
+},err=>{
+  return Promise.reject(err)
+})
+//响应拦截
+http.interceptors.response.use(res=>{
+  return res
+},err=>{
+  if(err.response.data.message) {
+    // $message是elementui的一种方法,通用的错误代理
+    Vue.prototype.$message({
+      type:"error",
+      message:err.response.data.message
+    })
+    if(err.response.status === 401) {
+      router.push('/login')
+    }
+  }
+  return Promise.reject(err)
+})
 export default http
