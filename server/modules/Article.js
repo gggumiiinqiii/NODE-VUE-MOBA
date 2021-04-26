7a25c981e8ef3ea5f2576a050d2394b0a6ca3bde
gggumiiinqiii <1378827345@qq.com>
2021-04-26 22:13:38 +0800

    feat 数据添加时加上时间戳

--- server/modules/Article.js
+++ server/modules/Article.js
@@ -4,6 +4,9 @@ const scheme = new mongoose.Schema({
   title:{type:String},
   content:{type:String},
   categories:[{type:mongoose.SchemaTypes.ObjectId,ref:'Category'}]
+},{
+  //录入数据的时候自动加时间戳
+  timestamps:true
 })
 
 module.exports = mongoose.model('Article',scheme)
