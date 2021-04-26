6adc623b76a2a0c6c950a6deb94b7cfc17222110
gggumiiinqiii <1378827345@qq.com>
2021-04-26 22:13:56 +0800

    feat 添加虚拟virtual

--- server/modules/Category.js
+++ server/modules/Category.js
@@ -5,5 +5,16 @@ const schema = new mongoose.Schema({
   // 关联自己本身的模型
   parent:{type:mongoose.SchemaTypes.ObjectId,ref:'Category'}
 })
-
+schema.virtual('children',{
+  localField:'_id',
+  foreignField:'parent',
+  justOne: false,
+  ref:'Category'
+})
+schema.virtual('newsList', {
+  localField: '_id',
+  foreignField: 'categories',
+  justOne: false,
+  ref: 'Article'
+})
 module.exports = mongoose.model('Category',schema)
