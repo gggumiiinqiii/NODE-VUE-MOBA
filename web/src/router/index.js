88e9a4187679832b196455e3454c1d4708b59ab0
gggumiiinqiii <1378827345@qq.com>
2021-04-28 13:37:07 +0800

    style 图片和字体图标

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import Home from '../views/Home.vue'
+import Article from '../views/Article.vue'
 
 Vue.use(VueRouter)
 
@@ -11,8 +12,18 @@ const routes = [
     name: 'main',
     component: Main,
     children:[
-      {path:'/',name:'home',component:Home}
+      {path:'/',name:'home',component:Home},
+      {path:'/articles/:id',name:'article',component:Article,props:true},//props：true表示传入的参数被当作props处理
     ]
+  },
+  {
+    path: '/heroes/:id',
+    name: 'Hero',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Hero.vue'),
+    props:true
   },
   {
     path: '/about',
