9a8f2fcfb1038f1da5a7dbdd062d8f03c10faac8
gggumiiinqiii <1378827345@qq.com>
2021-04-28 13:29:43 +0800

    feat 英雄编辑添加内容

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -3,7 +3,7 @@
     <h1>{{id?'编辑':'新建'}}英雄</h1>
                                               <!--.native是对一个组件绑定系统的原生事件，prevent是提交以后不刷新页面  -->
     <el-form label-width="120px"  @submit.native.prevent="save">
-      <el-tabs value="skills" type="border-card">
+      <el-tabs value="basic" type="border-card">
         <el-tab-pane label="基本信息" name="basic">
           <el-form-item label="名称">
             <el-input v-model="model.name"></el-input>
@@ -17,8 +17,19 @@
               :action="$http.defaults.baseURL+'/upload'"
               :headers="getAuthHeaders()"
               :show-file-list="false"
-              :on-success="afterupload">
-              <img v-if="model.avator" :src="model.avator" class="avatar">
+              :on-success="res=>$set(model,'avatar',res.url)">
+              <img v-if="model.avatar" :src="model.avatar" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="背景图" >
+            <el-upload
+              class="avatar-uploader"
+              :action="$http.defaults.baseURL+'/upload'"
+              :headers="getAuthHeaders()"
+              :show-file-list="false"
+              :on-success="res=>$set(model,'banner',res.url)">
+              <img v-if="model.banner" :src="model.banner" class="avatar">
               <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
           </el-form-item>
@@ -84,13 +95,19 @@
               <el-form-item label="名称">
                 <el-input v-model="item.name"></el-input>
               </el-form-item>
+              <el-form-item label="冷却值">
+                <el-input v-model="item.delay"></el-input>
+              </el-form-item>
+              <el-form-item label="消耗">
+                <el-input v-model="item.cost"></el-input>
+              </el-form-item>
               <el-form-item label="图标">
                 <el-upload
                   class="avatar-uploader"
                   :action="$http.defaults.baseURL+'/upload'"
                   :headers="getAuthHeaders()"
                   :show-file-list="false"
-                  :on-success="res=>item.icon=res.url">
+                  :on-success="res=>$set(item,'icon',res.url)">
                   <img v-if="item.icon" :src="item.icon" class="avatar">
                   <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                 </el-upload>
@@ -104,6 +121,31 @@
               <el-form-item>
                 <!-- splice()修改器，删除数组元素 -->
                 <el-button size="small" type="danger" @click="model.skills.splice(i,1)">删除</el-button>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-tab-pane>
+        <el-tab-pane label="搭档信息" name="partners">
+          <el-button @click="model.partners.push({})"><i class="el-icon-plus"></i>添加英雄</el-button>
+          <el-row  type="flex" style="flex-wrap:wrap"> 
+            <el-col :md="12" v-for="(item,i) in model.partners" :key="i">
+              <el-form-item label="英雄">
+                <el-select filterable v-model="item.hero">
+                  <el-option 
+                  v-for="item of heroes"
+                  :key="item._id"
+                  :label="item.name"
+                  :value="item._id"
+                  >
+                  </el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="描述">
+                <el-input v-model="item.description" type="textarea"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <!-- splice()修改器，删除数组元素 -->
+                <el-button size="small" type="danger" @click="model.partners.splice(i,1)">删除</el-button>
               </el-form-item>
             </el-col>
           </el-row>
@@ -125,6 +167,7 @@
       return {
         categories:[],
         items:[],
+        heroes:[],
         model:{
           name:'',
           avatar:'',
@@ -144,7 +187,8 @@
             name:'',
             description:'',
             tips:''
-          }]
+          }],
+          partners:[]
         }
       }
     },
@@ -160,9 +204,9 @@
     methods:{
       afterupload(res){
         //vue提供的显示赋值
-        //this.$set(this.model,'avator',res.url)
+        //this.$set(this.model,'avatar',res.url)
         //这个虽然值会存在，但是因为一开始data没有定义所以无法显示
-         this.model.avator = res.url
+         this.model.avatar = res.url
       },
       // 新建一条记录的话用post
       //更新一条记录的话用put
@@ -199,11 +243,16 @@
       async fetchItems(){
         const res = await this.$http.get(`rest/items`)
         this.items = res.data
+      },
+      async fetchHeroes(){
+        const res = await this.$http.get(`rest/heroes`)
+        this.heroes = res.data
       }
     },
     created(){
       this.fetchItems()
       this.fetchCategories()
+      this.fetchHeroes()
       // 两个&&表示并且前面的条件满足,执行后面的方法/
       //如果第一个运算子的布尔值为true，则返回第二个运算子的值（注意是值，不是布尔值）；
       //如果第一个运算子的布尔值为false，则直接返回第一个运算子的值，且不再对第二个运算子求值。
