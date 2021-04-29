8fc55c11dabc95c815de90189a0f307820f24d37
gggumiiinqiii <1378827345@qq.com>
2021-04-29 16:23:44 +0800

    feat 打包到服务器的文件

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ app.use(require('cors')())
 //在admin/index.js await 中req.body使用
 app.use(express.json())
 //托管静态文件
+app.use('/admin',express.static(__dirname+'/admin'))
+app.use('/',express.static(__dirname+'/web'))
 app.use('/uploads',express.static(__dirname+'/uploads'))
 // 数据库的连接
 require('./plugins/db.js')(app)
